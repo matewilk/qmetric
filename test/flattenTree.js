874ec42f362bf62e640fa30c3893535095e72625
Mateusz Wilk <matewilk@gmail.com>
2017-05-30 16:21:16 +0100

    getting familiar with ava

--- test/flattenTree.js
+++ test/flattenTree.js
@@ -1,3 +1,12 @@
 const test = require('ava');
 
+const flattenTree = require('../lib/flattenTree');
+const answers = require('../examples/answers');
+const output = require('../examples/expectedOutput');
+const questionset = require('../examples/questionset');
+
 test.todo('flattens correctly the provided examples');
+
+test('flatten tree should return a correct answer', t => {
+    t.is(flattenTree(questionset, answers), [questionset[0], questionset[1]]);
+});
