3b948df672ecb175c83139dad2fd5b33bd97a98f
Mateusz Wilk <matewilk@gmail.com>
2017-06-01 11:50:04 +0100

    Specifying requrements with ava

--- test/flattenTree.js
+++ test/flattenTree.js
@@ -1,12 +1,34 @@
 const test = require('ava');
 
-const flattenTree = require('../lib/flattenTree');
 const answers = require('../examples/answers');
 const output = require('../examples/expectedOutput');
 const questionset = require('../examples/questionset');
+
+const { flattenTree, parser } = require('../lib/flattenTree');
+
 
 test.todo('flattens correctly the provided examples');
 
 test('flatten tree should return a correct answer', t => {
     t.is(flattenTree(questionset, answers), [questionset[0], questionset[1]]);
+});
+
+test('should match answers with questionset ids', t => {
+    t.is(parser(), '123', '123');
+});
+
+test('should be able to recognise the type of the question', t => {
+
+});
+
+test('should recursively parse question object', t => {
+
+});
+
+test('should look for a question trigger', t => {
+
+});
+
+test('should build an output object on each level', t => {
+
 });
